959588b9a3df5f2c4506eddcca4f72a272021ccb
Varshitha R <varshitha.rn.0508@gmail.com>
2026-06-21 07:42:05 +0530

    Initial

---
+++ src/pages/lobby.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function Lobby(){
+    const [playerName,setPlayerName]=useState("")
+    const [joined,setJoined]=useState(false)
+    const [playersInLobby,setPlayersInLobby]=useState([])
+    let params= useParams();
+
+    useEffect(()=>{
+        const  name=localStorage.getItem(`lobby_${params.lobbyId}_player`)
+        if (name){
+            setPlayerName(name)
+            setJoined(true)
+            
+        }
+    },[])
+    
+    
+    function join_lobby(){
+        localStorage.setItem(`lobby_${params.lobbyId}_player`,"anna")
+        fetch(`http://127.0.0.1:8000/join_lobby/${params.lobbyId}`,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        player_name:playerName
+      })
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log("Response:",data)
+    
+setJoined(true);
+get_lobby();})  
+    .catch(err=>console.error(err))
+    }
+
+    function get_lobby(){
+      fetch(`http://127.0.0.1:8000/get_lobbies/${params.lobbyId}`,{
+      method:"GET",
+      headers:{
+        "Content-Type":"application/json"
+      }
+     
+      
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log("Response:",data)
+    
+})  
+    .catch(err=>console.error(err))
+
+    }
+    function start_game(){
+      fetch(`http://127.0.0.1:8000/start_game/${params.lobbyId}`,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+       body:JSON.stringify({
+        players:playersInLobby
+      })
+      
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log("Response:",data)
+    
+      setPlayersInLobby(data.players)})  
+    .catch(err=>console.error(err))
+
+
+    }
+    return (<div>
+        
+        {!joined ?(
+            <div>
+            <input
+              type="text"
+              value={playerName}
+              placeholder="Enter Player Name"
+              onChange={(e)=>setPlayerName(e.target.value)
+                
+              }
+            />
+            
+            </div>
+        ) :
+        (<div><h1> HANUMAN LOBBY {params.lobbyId}</h1>
+          {playersInLobby.map((element,ind)=>(
+            <h3 key={ind}> {element} </h3>
+          ))
+        }
+        <button onClick={start_game}>START GAME</button>
+        </div>
+        )
+        }
+        <button onClick={join_lobby}>Join Lobby</button>
+        <h1>LOBBY {params.lobbyId}</h1> </div>);
+}
+
---
+++ src/pages/veedu.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export default function Home(){
+const [inLobby,setInLobby]=useState(false)
+const [lobbyID,setLobbyID]=useState(0)
+let navigate=useNavigate()
+function createLobby(){
+  fetch("http://127.0.0.1:8000/create_lobbies",{
+      method:"GET",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log("Response:",data)
+      setLobbyID(data.lobby_id)
+      setInLobby(true)
+      navigate(`/lobby/${data.lobby_id}`)
+
+      //setGameStarted(true)
+
+      // ✅ Initialize positions using GLOBAL player IDs
+     
+    })
+    .catch(err=>console.error(err))
+  }
+
+
+
+    return (<div className="lobby">
+          return <h1>HANUMAN</h1>
+          <button onClick={createLobby}>CREATE LOBBY</button>
+        </div>)
+   ;
+}
